89bfa9a9dbf7ede9054701697dcb1db9466d3272
Akash Manwar <akashmanwar@Akashs-MacBook-Air.local>
2022-09-21 23:12:46 +0530

    Fixed code

--- primeNumber.js
+++ primeNumber.js
@@ -1,8 +1,8 @@
 var num;
 function Prime(num){
-    var count=0;
-    for(var i=2;i<num;i++){
-        if(num%i==0){
+    let count=0;
+    for(let i=2;i<num;i++){
+        if(num%i===0){
             count++;
         }
     }
@@ -14,4 +14,4 @@ function Prime(num){
     }
 }
 
-Prime(10);
+Prime(77);
